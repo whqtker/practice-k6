1b8f90ae147b05c3fbb375d5ae3c169c1081c6eb
seonghyeok <chosh2001@naver.com>
2025-05-09 13:35:04 +0900

    init

---
+++ src/main/java/org/ll/practicek6/PracticeK6Application.java
@@ -0,0 +1,13 @@
+package org.ll.practicek6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeK6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeK6Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ll/practicek6/PracticeK6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ll.practicek6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeK6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
