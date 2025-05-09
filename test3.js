2c9b599acf4887d0b86869974fca6291dc6da82c
seonghyeok <chosh2001@naver.com>
2025-05-09 14:10:07 +0900

    feat: k6 테스트 3

---
+++ test3.js
@@ -0,0 +1,16 @@
+// K6_WEB_DASHBOARD=true k6 run test3.js: 테스트 과정을 웹 대시보드에서 볼 수 있음
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+// 3분 동안 7000개의 vu가 요청을 점진적으로 보냄
+export const options = {
+    stages: [
+        { duration: '3m', target: 7000 }
+    ],
+};
+
+export default function () {
+    http.get('http://localhost:8080/api/v1/posts');
+    sleep(1);
+}
